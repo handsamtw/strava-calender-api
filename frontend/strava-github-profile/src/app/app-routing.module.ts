c05456832cf76673669bbf3ab181f4f3ef42fa4a
Shousan Liao <samliao980822@gmail.com>
2023-12-23 02:35:34 -0500

    initial commit to check deployment work as expect

--- frontend/strava-github-profile/src/app/app-routing.module.ts
+++ frontend/strava-github-profile/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+const routes: Routes = [{ path: 'dashboard', component: DashboardComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
